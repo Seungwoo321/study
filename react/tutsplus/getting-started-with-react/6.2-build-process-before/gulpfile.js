6836f754f479cf18abb0ff380522bad443413f96
Seungwoo Lee <swlee@mz.co.kr>
2020-03-11 22:14:16 +0900

    remove private resource

--- react/tutsplus/getting-started-with-react/6.2-build-process-before/gulpfile.js
+++
@@ -1,6 +0,0 @@
-var gulp = require('gulp'),
-    connect = require('gulp-connect'),
-    open = require("gulp-open"),
-    browserify = require('gulp-browserify'),
-    concat = require('gulp-concat'),
-    port = process.env.port || 3031;
--- react/tutsplus/getting-started-with-react/6.3-static/app/src/js/main.js
+++
@@ -1,8 +0,0 @@
-/** @jsx React.DOM */
-var React = require('react'),
-    Feed = require('./components/Feed');
-
-React.renderComponent(
-  <Feed />,
-  document.getElementById('app')
-);
