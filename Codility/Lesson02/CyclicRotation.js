dbb9aed993bd703ff9408228488fd2cc244c3955
MZC02-SWLEE <mzc02-swlee@MZC02-SWLEE.local>
2021-05-01 23:29:35 +0900

    update lesson 2

---
+++ Codility/Lesson02/CyclicRotation.js
@@ -0,0 +1,14 @@
+
+function solution (A, K) {
+    if (A.length - 1 === K) {
+        return A
+    } else {
+        ``
+    }
+
+
+}
+
+console.log(solution([3, 8, 9, 7, 6], 3)) // [9, 7, 6, 3, 8]
+console.log(solution([0, 0, 0], 1)) // [0, 0, 0]
+console.log(solution([1, 2, 3, 4], 4)) // [1, 2, 3, 4]
