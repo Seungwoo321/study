98d0cf80e012f4729f3994fdbf9804310a56cac5
swlee <swlee@mz.co.kr>
2021-05-14 18:43:55 +0900

    update lesson_64064

--- programmers/2019_카카오_개발자_겨울_인턴십/lessons_64064.js
+++ programmers/2019_카카오_개발자_겨울_인턴십/lessons_64064.js
@@ -1,34 +1,37 @@
 // 불량 사용자
 
 function solution(userId, bannedId) {
-    let answer = 0
+    let answer = []
+    let i = 0
+    while (i < bannedId.length) {
+        const pattern = bannedId[i].replace(/\*/g, '.')
+        const regExp = new RegExp(pattern)
+        let filterIds = userId.filter(id => regExp.test(id) && id.length === bannedId[i].length).map(id => [id])
+        if (!answer.length) {
+            answer = answer.concat(filterIds)
+        } else {
+            answer = filterIds.reduce((accumulator, currentValue) => {
+                const current = answer.map(arr => {
+                    const filteredItem = arr.filter(item => !item.includes(currentValue))
+                    return currentValue.concat(filteredItem).sort()
+                }).filter(item => item.length === i + 1)
 
-    while (bannedId.length) {
-        const firstBanned = [...bannedId.shift()]
+                if (accumulator.filter(arr => arr.join('') !== current.join('')).length) {
 
-        const pattern = firstBanned.map(s => {
-            if (s === '*') {
-                return '[A-Za-z0-9]'
-            } else {
-                return s
-            }
-        }).join('')
-        
-        const re = new RegExp(pattern)
-        let count = 0
-        userId.forEach(id => {
-            if (re.test(id) && firstBanned.length === id.length) {
-                count ++;
-            }
-        })
-        answer = Math.max.apply(null, [answer, count])
-
+                    return accumulator.concat(current).sort()
+                } else {
+                    return accumulator.concat(current).sort()
+                }
+                // return accumulator.filter(arr => arr.join('') !== current.join('')).concat(current).sort()
+            }, [])
+        }
+        i ++
     }
     return answer
-
 }
 
 
-console.log(solution(["frodo", "fradi", "crodo", "abc123", "frodoc"], ["fr*d*", "abc1**"])) // 2 //
+// console.log(solution(["frodo", "fradi", "crodo", "abc123", "frodoc"], ["fr*d*", "abc1**"])) // 2 //
 console.log(solution(["frodo", "fradi", "crodo", "abc123", "frodoc"], ["*rodo", "*rodo", "******"])) // 2
-console.log(solution(["frodo", "fradi", "crodo", "abc123", "frodoc"], ["fr*d*", "*rodo", "******", "******"])) // 3
+// console.log(solution(["frodo", "fradi", "crodo", "abc123", "frodoc"], ["fr*d*", "*rodo", "******", "******"])) // 3
+// console.log(solution(["frodo", "fradi", "crodo", "abc123", "frodoc"], ["fr*d*", "*rodo"]))
